e63455c35e3b075138214a801d0500b8c5e95d3a
saiyvoud <76910592+saiyvoud@users.noreply.github.com>
2023-06-16 22:03:59 +0700

    add

--- lib/pages/home/detail_home.dart
+++ lib/pages/home/detail_home.dart
@@ -1,5 +1,4 @@
 import 'package:course_mobile/components/colors.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class DetailHome extends StatefulWidget {
@@ -14,42 +13,171 @@ class _DetailHomeState extends State<DetailHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
-      // appBar: AppBar(
-      //   backgroundColor: primaryColorWhite,
-      //   elevation: 0,
-      //   centerTitle: true,
-      //   title: Text(
-      //     "Detail Product",
-      //     style: TextStyle(fontSize: 18, color: primaryColorBlack),
-      //   ),
-      //   leading: IconButton(
-      //     icon: Icon(
-      //       Icons.arrow_back_ios,
-      //       color: primaryColorBlack,
-      //     ),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //     },
-      //   ),
-      // ),
-    
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset(
-              "assets/images/product5.png",
-              width: double.infinity,
-             fit: BoxFit.cover,
-            ),
-            SizedBox(height: 10),
             Container(
-              height: 200,
+              height: 400,
+              //decoration: BoxDecoration(color: primaryColorGreen),
+              child: Stack(
+                children: [
+                  Image.asset(
+                    "assets/images/product5.png",
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: 10,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: primaryColorWhite,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 3,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ]),
+                        child: Center(
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            color: primaryColorBlack,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 30,
+                    right: 10,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: primaryColorWhite,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ]),
+                      child: Center(
+                        child: Icon(
+                          Icons.shopping_bag,
+                          color: primaryColorBlack,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 400,
               decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 6,
+                      blurRadius: 10,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "IPhone 14 Pro Max",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "TKGirl ใหม่ล่าสุด รองเท้าแตะน้องฉลามสีเรนโบว์มีให้เลือก ถึง 3แบบ🏳️‍🌈 🐳 สีสันสดใส พาสเทลบาดจุยมากแม่ จึ้งงงง",
+                        style: TextStyle(fontSize: 15, color: Colors.grey),
+                      ),
+                      SizedBox(height: 10),
+                      Divider(
+                        indent: 50,
+                        endIndent: 50,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "LAK 20,000,000",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: primaryOrange),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Center(
+                            child: Container(
+                              height: 50,
+                              width: MediaQuery.of(context).size.width / 2.2,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.amber,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "ສັ່ງຊື້ເລີຍ",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: primaryColorWhite,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: Container(
+                              height: 50,
+                              width: MediaQuery.of(context).size.width / 2.2,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: primaryColorGreen,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "ໂທສອບຖາມ",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: primaryColorWhite,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ]),
               ),
             )
           ],
